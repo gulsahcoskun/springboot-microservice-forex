7bc63d25ad08fb7044ec228fc2d1d3e3cc8e1ef1
gulsah <gulsahcoskunn@gmail.com>
2018-01-30 22:51:36 +0300

    initial commit

---
+++ ExchangeValueRepository.java
@@ -0,0 +1,7 @@
+package com.gulsah.project.microservice.forex.springbootmicroserviceforex;
+
+/**
+ * Created by GulsahCoskun on 30.01.2018.
+ */
+public class ExchangeValueRepository {
+}
---
+++ SpringbootMicroserviceForexApplication.java
@@ -0,0 +1,12 @@
+package com.gulsah.project.microservice.forex.springbootmicroserviceforex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceForexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroserviceForexApplication.class, args);
+	}
+}
