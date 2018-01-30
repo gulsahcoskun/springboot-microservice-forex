25dbd66b29fcb0cfb94fa4046fb73d77128e5539
gulsah <gulsahcoskunn@gmail.com>
2018-01-30 23:19:28 +0300

    forex service completed

--- ExchangeValue.java
+++ ExchangeValue.java
@@ -1,7 +1,60 @@
 package com.gulsah.project.microservice.forex.springbootmicroserviceforex;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigInteger;
+
 /**
  * Created by GulsahCoskun on 30.01.2018.
  */
+@Entity
 public class ExchangeValue {
+
+    @Id
+    private Long id;
+
+    @Column(name="currency_from")
+    private String from;
+
+    @Column(name="currency_to")
+    private String to;
+
+    private BigInteger conversionMultiple;
+    private int port;
+
+    public ExchangeValue() {
+    }
+
+    public ExchangeValue(Long id, String from, String to, BigInteger conversionMultiple, int port) {
+        super();
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigInteger getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
 }
--- ExchangeValueRepository.java
+++ ExchangeValueRepository.java
@@ -1,7 +1,11 @@
 package com.gulsah.project.microservice.forex.springbootmicroserviceforex;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Created by GulsahCoskun on 30.01.2018.
  */
-public class ExchangeValueRepository {
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue,Long> {
+
+    ExchangeValue findByFromAndTo(String from,String to);
 }
